d629dd48a24eb2c97950e79a9d98c2edd04c5eb3
Felipe Henao <fahenao@MacBook-Pro-de-Felipe.local>
2015-08-26 08:55:13 +0200

    To Do app second iteration

---
+++ dayTwo/to_do_sinatra/to_do.rb
@@ -0,0 +1,3 @@
+class Task
+  status = nil
+end
--- dayTwo/to_do_sinatra/to_do_app.rb
+++ dayTwo/to_do_sinatra/to_do_app.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
-
+require_relative './to_do.rb'
 enable :sessions
 
+todo_list = ["Do laundry", "Walk the dog", "Go to the Mercadona"]
+
 get '/' do
+  @list = todo_list
   erb :to_do
+end
+
+post '/add_new' do
+  new_task = params[:new_task]
+  todo_list.push(params[:new_task])
+  redirect to("/")
 end
